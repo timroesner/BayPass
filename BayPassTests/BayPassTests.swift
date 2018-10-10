cea7a52e680a94172935d024cc6bb441937e519b
Ayesha Khan <akhan1231@hotmail.com>
2018-10-09 19:25:16 -0700

    Swift Format Installed

--- BayPassTests/BayPassTests.swift
+++ BayPassTests/BayPassTests.swift
@@ -6,11 +6,10 @@
 //  Copyright © 2018 Tim Roesner. All rights reserved.
 //
 
-import XCTest
 @testable import BayPass
+import XCTest
 
 class BayPassTests: XCTestCase {
-
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -26,9 +25,8 @@ class BayPassTests: XCTestCase {
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
