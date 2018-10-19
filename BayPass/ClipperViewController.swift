c79875480b442d99cd8ede2002630bf0189f4f5e
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2018-10-18 21:35:33 -0700

    Add ClipperViewController

---
+++ BayPass/ClipperViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ClipperViewController.swift
+//  BayPass
+//
+//  Created by Tim Roesner on 10/18/18.
+//  Copyright © 2018 Tim Roesner. All rights reserved.
+//
+
+import UIKit
+
+class ClipperViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
