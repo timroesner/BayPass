15b82ca82402ce56dd61e8c99ea7427ec4d24d54
Ayesha Khan <akhan1231@hotmail.com>
2018-10-20 17:29:07 -0700

    Some of the classes from the UML Diagram

---
+++ BayPass/Agency.swift
@@ -0,0 +1,18 @@
+//
+//  Agency.swift
+//  BayPass
+//
+//  Created by Ayesha Khan on 10/20/18.
+//  Copyright © 2018 Tim Roesner. All rights reserved.
+//
+
+import UIKit
+
+struct Agency {
+    var name: String
+    var routes: [Line]
+
+    func getRoutes() -> [Line] {
+        return []
+    }
+}
