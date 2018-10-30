87923da8b14197cb93ff8d30cb2927a5b76a33e0
timroesner <13894518+timroesner@users.noreply.github.com>
2018-10-29 23:24:32 -0700

    Add instance variables to BikeDock
    This change is necessary to create MapAnnotations from a BikeDock. I also optioned to include these statics in the struct this will not break the tests once we change the color

--- BayPass/Model/Map/BikeDock.swift
+++ BayPass/Model/Map/BikeDock.swift
@@ -10,10 +10,16 @@ import CoreLocation
 import UIKit
 
 struct BikeDock {
+    var name: String
     var location: CLLocation
     var bikesAvailible: Int
 
-    init(location: CLLocation, bikesAvailible: Int) {
+    // Only used for the MapAnnotation
+    var color = #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)
+    var icon = UIImage(named: "Bike") ?? UIImage()
+
+    init(name: String, location: CLLocation, bikesAvailible: Int) {
+        self.name = name
         self.location = location
         self.bikesAvailible = bikesAvailible
     }
