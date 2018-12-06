2d3cad127b567628a2bb37e3e9cbb7179754e268
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2018-12-05 22:32:30 -0800

    Add credentials as output file to build script

--- create-credentials.rb
+++ create-credentials.rb
@@ -7,8 +7,6 @@ struct Credentials {
     let birdToken = "#{ENV['BIRD_TOKEN']}"
 }
 CREDS_FILE_STRING
-print("Created File content")
 file = File.new("./BayPass/APIs/Credentials.swift", "w")
 file.puts(file_content)
 file.close
-print("Wrote File")
