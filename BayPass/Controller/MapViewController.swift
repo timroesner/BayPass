a89efc1c0b54e3cf18f7b44f0c88af35e937a09d
Ayesha Khan <akhan1231@hotmail.com>
2019-01-26 15:13:06 -0800

    Got data for Stations

--- BayPass/Controller/MapViewController.swift
+++ BayPass/Controller/MapViewController.swift
@@ -17,6 +17,52 @@ struct GTFSOperators: Decodable {
     let LastGenerated: String // Don't need this
 }
 
+/*
+ id": "12TH",
+ "Name": "12th St. Oakland City Center BART Station",
+ "Location": {
+ "Longitude": "-122.27145",
+ "Latitude": "37.803768"
+ },
+ "StopType": "onstreetBus"
+ */
+struct StationInfo: Decodable {
+    let id: String
+    let Name: String
+    enum Location: String, CodingKey {
+        case Location
+    }
+
+    let StopType: String
+}
+
+struct Stations: Decodable {
+    let stations: [StationInfo]
+
+    enum CodingKeys: String, CodingKey {
+        case contents = "Contents"
+    }
+
+    enum DataObjects: String, CodingKey {
+        case dataObjects
+    }
+
+    enum ScheduledStopPoint: String, CodingKey {
+        case scheduledStopPoint = "ScheduledStopPoint"
+    }
+
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        let dataObjects = try values.nestedContainer(keyedBy: DataObjects.self,
+                                                     forKey: .contents)
+        let scheduledStopPoint = try dataObjects.nestedContainer(keyedBy: ScheduledStopPoint.self,
+                                                                 forKey: .dataObjects)
+
+        stations = try scheduledStopPoint.decode([StationInfo].self,
+                                                 forKey: .scheduledStopPoint)
+    }
+}
+
 class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,5 +78,29 @@ class MapViewController: UIViewController {
             make.width.equalTo(350)
             make.height.equalTo(200)
         })
+    }
+
+    // Station
+    func loadJSONForStation() {
+        // alamofire use instead
+        let jsonUrlString = "https://api.511.org/transit/stops?api_key=11f56fe7-a97a-40db-9d94-130584c8bac6&operator_id=BA"
+        guard let url = URL(string: jsonUrlString) else { return }
+
+        URLSession.shared.dataTask(with: url) { data, _, _ in
+            print("test2")
+
+            guard let data = data else { return }
+            do {
+                let stations = try JSONDecoder().decode(Stations.self, from: data) // parsing filter out based on id and name
+                print("\(stations.stations)\n")
+
+                for st in stations.stations {
+                    print("\(st)\n")
+                }
+
+            } catch let jsonErr {
+                print("Error serializing json:", jsonErr)
+            }
+        }.resume()
     }
 }
