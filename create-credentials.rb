84c8a488091420a64c704fad0bf261f0d825c982
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-02-01 21:47:04 -0800

    Add input file

--- create-credentials.rb
+++ create-credentials.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/ruby
 
-#file_content = <<-CREDS_FILE_STRING
-#import Foundation
-#
-#struct Credentials {
-#    let birdToken = "#{ENV['BIRD_TOKEN']}"
-#}
-#CREDS_FILE_STRING
-#file = File.new("./BayPass/APIs/Credentials.swift", "w")
-#file.puts(file_content)
-#file.close
+file_content = <<-CREDS_FILE_STRING
+import Foundation
+
+struct Credentials {
+   let birdToken = "#{ENV['BIRD_TOKEN']}"
+}
+CREDS_FILE_STRING
+file = File.new("./BayPass/APIs/Credentials.swift", "w")
+file.puts(file_content)
+file.close
