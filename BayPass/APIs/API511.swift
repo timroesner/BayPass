6b206267db71f7cd4134dd48cdad3d3154d70779
Ayesha Khan <akhan1231@hotmail.com>
2019-02-03 15:45:46 -0800

    added alamofire and added another 511 api swift file

---
+++ BayPass/APIs/API511.swift
@@ -0,0 +1,16 @@
+//
+//  511.swift
+//  BayPass
+//
+//  Created by Ayesha Khan on 2/3/19.
+//  Copyright © 2019 Tim Roesner. All rights reserved.
+//
+
+import Alamofire
+import Foundation
+
+class API511 {
+    init() {}
+
+    func getAgencies() {}
+}
