1678c06c92e3c90a8d6718170a1b62353a76ca01
Ayesha Khan <akhan1231@hotmail.com>
2019-02-12 21:42:18 -0800

    new structure for calling 511

---
+++ BayPass/GTFS/APIError.swift
@@ -0,0 +1,24 @@
+//
+//  APIError.swift
+//  BayPass
+//
+//  Created by Ayesha Khan on 2/11/19.
+//  Copyright © 2019 Tim Roesner. All rights reserved.
+//
+
+enum APIError: Error {
+    case requestFailed
+    case jsonConversionFailure
+    case invalidData
+    case responseUnsuccessful
+    case jsonParsingFailure
+    var localizedDescription: String {
+        switch self {
+        case .requestFailed: return "Request Failed"
+        case .invalidData: return "Invalid Data"
+        case .responseUnsuccessful: return "Response Unsuccessful"
+        case .jsonParsingFailure: return "JSON Parsing Failure"
+        case .jsonConversionFailure: return "JSON Conversion Failure"
+        }
+    }
+}
