bc1966191454e0af66f9d167f86fd9efb9371900
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-02-27 17:46:37 -0800

    Add sample table View to MapsViewController

--- BayPass/Controller/MapViewController.swift
+++ BayPass/Controller/MapViewController.swift
@@ -10,10 +10,31 @@ import CoreLocation
 import SnapKit
 import UIKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let cellIdentifier = "station"
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let stations = [Station(name: "Santa Clara Station", code: 922, transitModes: [.bus, .lightRail], lines: ["922", "323", "923", "23", "181"], location: CLLocation(latitude: 0.0, longitude: 0.0)), Station(name: "San Jose Diridon", code: 112, transitModes: [.bus, .lightRail, .calTrain], lines: ["Cal", "922", "923", "17", "64"], location: CLLocation(latitude: 0.0, longitude: 0.0)), Station(name: "7th & San Fernando", code: 922, transitModes: [.bus], lines: ["64", "72", "73", "81"], location: CLLocation(latitude: 0.0, longitude: 0.0))]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! StationSearchResultTableViewCell
+        cell.setup(with: stations[indexPath.row])
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         addLabel(title: "Map")
+        
+        let tableView = UITableView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 250))
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(StationSearchResultTableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        self.view.addSubview(tableView)
     }
 }
