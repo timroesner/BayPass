40d2916b10b54c12df2668638f28ea92f2b7135b
Ayesha Khan <akhan1231@hotmail.com>
2019-03-01 17:18:36 -0800

    moved uicolor extentions and added to model for line

--- BayPass/APIs/Here.swift
+++ BayPass/APIs/Here.swift
@@ -326,28 +326,3 @@ class Here {
         print("\(name) \(destination) \(color) ")
     }
 }
-}
-
-extension UIColor {
-    convenience init?(hexString: String) {
-        var chars = Array(hexString.hasPrefix("#") ? hexString.dropFirst() : hexString[...])
-        let red, green, blue, alpha: CGFloat
-        switch chars.count {
-        case 3:
-            chars = chars.flatMap { [$0, $0] }
-            fallthrough
-        case 6:
-            chars = ["F", "F"] + chars
-            fallthrough
-        case 8:
-            alpha = CGFloat(strtoul(String(chars[0 ... 1]), nil, 16)) / 255
-            red = CGFloat(strtoul(String(chars[2 ... 3]), nil, 16)) / 255
-            green = CGFloat(strtoul(String(chars[4 ... 5]), nil, 16)) / 255
-            blue = CGFloat(strtoul(String(chars[6 ... 7]), nil, 16)) / 255
-        default:
-            return nil
-        }
-        self.init(red: red, green: green, blue: blue, alpha: alpha)
-    }
-}
-
