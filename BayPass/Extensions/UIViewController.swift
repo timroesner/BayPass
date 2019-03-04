179d201f17b9252f8bf88582be2696294b169ff7
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-03-03 21:54:35 -0800

    Add UIViewController extension

---
+++ BayPass/Extensions/UIViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BayPass
+//
+//  Created by Tim Roesner on 3/2/19.
+//  Copyright © 2019 Tim Roesner. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func displayAlert(title: String, msg: String, dismissAfter: Bool){
+        let confirm = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+        confirm.addAction(UIAlertAction(title: "Ok", style: .cancel, handler:  { (action: UIAlertAction) in
+            if(dismissAfter) {
+                if let navController = self.navigationController {
+                    navController.popViewController(animated: true)
+                } else {
+                    self.dismiss(animated: true, completion: nil)
+                }
+            }
+        }))
+        
+        self.present(confirm, animated: true, completion: nil)
+    }
+}
