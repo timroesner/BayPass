8e792bef03419f701d6c6d215aa913a810471f31
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-03-03 21:53:49 -0800

    Add results tableView to SearchVC

--- BayPass/Controller/Map/SVCTableView.swift
+++ BayPass/Controller/Map/SVCTableView.swift
@@ -12,22 +12,31 @@ import MapKit
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searchResults.isEmpty {
+            tableView.backgroundView = EmptyView(text: "Nothing to see here")
+        } else {
+            tableView.backgroundView =  nil
+        }
         return searchResults.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let resultObject = searchResults[indexPath.row]
-        var cell = UITableViewCell()
         
         if let station = resultObject as? Station {
-            cell = tableView.dequeueReusableCell(withIdentifier: <#T##String#>, for: indexPath)
+            //cell = tableView.dequeueReusableCell(withIdentifier: <#T##String#>, for: indexPath)
         }
         
         if let destination = resultObject as? MKMapItem {
-            
+            let cell = tableView.dequeueReusableCell(withIdentifier: destinationCellId, for: indexPath) as! DestinationSearchResultTableViewCell
+            cell.setup(with: destination)
+            return cell
         }
-        return cell
+        return UITableViewCell()
     }
     
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        delegate?.didSelectSearchResult(searchResults[indexPath.row])
+    }
 }
