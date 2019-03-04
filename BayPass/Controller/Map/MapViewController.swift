1d93b33e250c5f8cb347fd7170f22f1bd07c05ff
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-03-03 21:52:15 -0800

    Add MapView

--- BayPass/Controller/Map/MapViewController.swift
+++ BayPass/Controller/Map/MapViewController.swift
@@ -12,17 +12,23 @@ import MapKit
 import OverlayContainer
 import UIKit
 
-class MapViewController: UIViewController, OverlayContainerViewControllerDelegate {
-    private var mapView = MKMapView()
-    let bottomSheet = OverlayContainerViewController()
+class MapViewController: UIViewController {
+    
+    private(set) var mapView = MKMapView()
+    let bottomSheet = OverlayContainerViewController(style: .rigid)
     let searchVC = SearchViewController()
+    let locationManager = CLLocationManager()
+    var notchPercentages = [CGFloat]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
+        setupLocation()
     }
     
     func setupViews() {
+        mapView.showsUserLocation = true
+        centerOnUserLocation()
         view.addSubview(mapView)
         mapView.snp.makeConstraints { (make) in
             make.top.bottom.right.left.equalToSuperview()
@@ -37,22 +43,8 @@ class MapViewController: UIViewController, OverlayContainerViewControllerDelegat
         
         bottomSheet.delegate = self
         searchVC.delegate = self
-        bottomSheet.viewControllers = [searchVC]
+        searchVC.parentMapVC = self
+        setupSearchView()
         addChild(bottomSheet, in: view)
-    }
-    
-    func numberOfNotches(in containerViewController: OverlayContainerViewController) -> Int {
-        return 2
-    }
-    
-    func overlayContainerViewController(_ containerViewController: OverlayContainerViewController, heightForNotchAt index: Int, availableSpace: CGFloat) -> CGFloat {
-        switch index {
-        case 0:
-            return availableSpace * 0.20
-        case 1:
-            return availableSpace * 0.95
-        default:
-            return 0
-        }
     }
 }
