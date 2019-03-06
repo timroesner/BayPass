85a8fbc98487d0dfdb9eabd4e8de58614e2327f2
thualing <tina.th.ling@gmail.com>
2019-03-06 10:16:16 -0800

    navigation controller rework
    new branch and add all files by using cocoa pod file

---
+++ BayPass/Controller/ClipperAddCashViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ClipperAddCashViewController.swift
+//  BayPass
+//
+//  Created by 凌脩羽 on 3/6/19.
+//  Copyright © 2019 Tim Roesner. All rights reserved.
+//
+
+import UIKit
+
+class ClipperAddCashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        title = "Add Cash Value"
+        navigationController?.navigationBar.prefersLargeTitles = false
+    }
+}
---
+++ BayPass/Controller/TicketCheckoutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TicketCheckoutViewController.swift
+//  BayPass
+//
+//  Created by 凌脩羽 on 3/6/19.
+//  Copyright © 2019 Tim Roesner. All rights reserved.
+//
+
+import UIKit
+
+class TicketCheckoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        title = "Ticket Checkout"
+        navigationController?.navigationBar.prefersLargeTitles = false
+    }
+}
