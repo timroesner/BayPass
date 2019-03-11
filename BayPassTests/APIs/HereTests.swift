f738b57c353257db1315f74a08cc138fa08bf368
Ayesha Khan <akhan1231@hotmail.com>
2019-03-11 12:32:58 -0700

    more tests

--- BayPassTests/APIs/HereTests.swift
+++ BayPassTests/APIs/HereTests.swift
@@ -34,7 +34,7 @@ class HereTests: XCTestCase {
             results = res
             ex.fulfill()
         }
-        wait(for: [ex], timeout: 30)
+        wait(for: [ex], timeout: 5)
         XCTAssertEqual(results!, [718_310_131])
     }
 
@@ -48,7 +48,40 @@ class HereTests: XCTestCase {
             result = resp
             ex.fulfill()
         }
-        wait(for: [ex], timeout: 30)
+        wait(for: [ex], timeout: 5)
         XCTAssertEqual(result!, Agency.BART)
+    }
+
+    func testGetStationsNearby() {
+        let ex = expectation(description: "Here for getting Station Ids")
+        let center = CLLocationCoordinate2D(latitude: 37.5032238, longitude: -121.9434281)
+        let radius = 1500
+        let max = 1
+        let station = Station(name: "Warm Springs/South Fremont", code: 718_310_131, transitModes: [TransitMode.bus], lines: [Line(name: "22", agency: Agency.VTA, destination: "22 Palo Alto", color: #colorLiteral(red: 0.2901960784, green: 0.5647058824, blue: 0.8862745098, alpha: 1), transitMode: TransitMode.bus)], location: CLLocation(latitude: 37.352516, longitude: -121.96947))
+
+        var results: [Station]? = [Station]()
+
+        Here().getStationsNearby(center: center, radius: radius, max: 1) { resp in
+            results = resp
+            ex.fulfill()
+        }
+        wait(for: [ex], timeout: 5)
+        XCTAssertNotNil(results!) // TODO: FIX
+//        XCTAssertEqual(results, [station])
+    }
+
+    func testGetLine() {
+        let ex = expectation(description: "Here for getting Line from a Station ID")
+        let stationId: Int = 718_310_131
+
+        var results: [Line]?
+        Here().getLine(stationId: stationId) { resp in
+            results = resp
+            ex.fulfill()
+        }
+
+        wait(for: [ex], timeout: 5)
+//        XCTAssertEqual(results!, )
+//        XCTAssertEqual(, )
     }
 }
