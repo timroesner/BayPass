94cc13740af7e07e5b23a3e052e86c2b5c8c5865
Ayesha Khan <akhan1231@hotmail.com>
2019-03-11 12:32:58 -0700

    more tests

--- BayPass/Extensions/String.swift
+++ BayPass/Extensions/String.swift
@@ -12,4 +12,11 @@ extension String {
     func localized() -> String {
         return NSLocalizedString(self, comment: "")
     }
+
+    func toDateTime() -> NSDate {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        let dateFromString: NSDate = dateFormatter.date(from: self)! as NSDate
+        return dateFromString
+    }
 }
