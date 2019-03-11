ad17c7c7f292f5ae309f6432446df1a3b092749c
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-03-10 20:31:06 -0700

    Remove debug comment

--- BayPass/Controller/Map/MapViewController.swift
+++ BayPass/Controller/Map/MapViewController.swift
@@ -27,7 +27,6 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
         setupViews()
         setupLocation()
-        // setupRoutesView(with: [Route(departureTime: Date(), arrivalTime: Date(timeIntervalSinceNow: 3000), segments: []), Route(departureTime: Date(), arrivalTime: Date(timeIntervalSinceNow: 6000), segments: [])])
     }
 
     func setupViews() {
