63ff4f09cdc5a81daeeaec9b87b444b15cc014d3
Zhe <zheli0705@gmail.com>
2019-03-12 11:11:55 -0700

    Add scroll view
    add scroll view

--- BayPass/Controller/TicketCheckoutViewController.swift
+++ BayPass/Controller/TicketCheckoutViewController.swift
@@ -15,5 +15,18 @@ class TicketCheckoutViewController: UIViewController {
         view.backgroundColor = .white
         title = "Ticket Checkout"
         navigationController?.navigationBar.prefersLargeTitles = false
+        
+        let ticketView = TicketView(agency: "BART", icon: UIImage(named: "BART")!, cornerRadius: 12)
+        view.addSubview(ticketView)
+        ticketView.snp.makeConstraints({ (make) -> Void in
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(20)
+            make.leading.equalToSuperview().inset(16)
+            make.trailing.equalToSuperview().inset(16)
+            make.height.equalTo(ticketView.snp.width).multipliedBy(0.6)
+        })
+        ticketView.layoutIfNeeded()
+        
+        //let ticketTypeDropDown = DropDownMenu(title: "ticket type", items: ["Apple Pay", "Credit/Debit", "Paypal"])
+        
     }
 }
--- BayPass/Controller/TicketViewController.swift
+++ BayPass/Controller/TicketViewController.swift
@@ -15,6 +15,31 @@ class TicketViewController: UIViewController {
         view.backgroundColor = .white
         title = "Tickets"
 
+        //scroll view
+        let scrollView : UIScrollView = UIScrollView(frame: CGRect(x: 0, y: 180, width: 350, height: 200))
+        view.addSubview(scrollView)
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.isPagingEnabled = true
+        scrollView.backgroundColor = .orange
+        let numberOfPages :Int = 5
+        let padding : CGFloat = 20
+        //let viewWidth = scrollView.frame.size.width - 2 * padding
+        let viewWidth = CGFloat(150)
+        let viewHeight = scrollView.frame.size.height - 2 * padding
+        
+        var x : CGFloat = 0
+        
+        for _ in 0...numberOfPages{
+            let view: UIView = UIView(frame: CGRect(x: x + padding, y: padding, width: viewWidth, height: viewHeight))
+            view.backgroundColor = UIColor.black
+            scrollView .addSubview(view)
+            
+            x = view.frame.origin.x + viewWidth + padding
+        }
+        
+        scrollView.contentSize = CGSize(width:x+padding, height:scrollView.frame.size.height)
+        
+        
         // button to ticket checkout page
         let button = UIButton()
         button.backgroundColor = UIColor(red: 221, green: 84, blue: 65)
