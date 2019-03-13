57f6bd03c8794c889ad725a0f78a8d239b1b1b49
Ayesha Khan <akhan1231@hotmail.com>
2019-03-12 21:12:31 -0700

    more tests

--- BayPassTests/APIs/HereTests.swift
+++ BayPassTests/APIs/HereTests.swift
@@ -14,8 +14,16 @@ import XCTest
 
 class HereTests: XCTestCase {
     var testJson: [String: Any] = [:]
+    var testJsonForAgency: [String: Any] = [:]
+
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let path = Bundle(for: type(of: self)).path(forResource: "Here", ofType: "json")
+        let data = try! Data(contentsOf: URL(fileURLWithPath: path!), options: .mappedIfSafe)
+        testJson = try! JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String: Any]
+
+        let pathForAgency = Bundle(for: type(of: self)).path(forResource: "HereAgency", ofType: "json")
+        let dataForAgency = try! Data(contentsOf: URL(fileURLWithPath: pathForAgency!), options: .mappedIfSafe)
+        testJsonForAgency = try! JSONSerialization.jsonObject(with: dataForAgency, options: .allowFragments) as! [String: Any]
     }
 
     override func tearDown() {
@@ -56,7 +64,7 @@ class HereTests: XCTestCase {
         let ex = expectation(description: "Here for getting Station Ids")
         let center = CLLocationCoordinate2D(latitude: 37.5032238, longitude: -121.9434281)
         let radius = 1500
-        let station = Station(name: "Warm Springs/South Fremont", code: 718_310_131, transitModes: [TransitMode.bus], lines: [Line(name: "22", agency: Agency.VTA, destination: "22 Palo Alto", color: #colorLiteral(red: 0.2901960784, green: 0.5647058824, blue: 0.8862745098, alpha: 1), transitMode: TransitMode.bus)], location: CLLocation(latitude: 37.352516, longitude: -121.96947))
+        let station = [Station(name: "Warm Springs/South Fremont", code: 718_310_131, transitModes: [TransitMode.bus], lines: [Line(name: "22", agency: Agency.VTA, destination: "22 Palo Alto", color: #colorLiteral(red: 0.2901960784, green: 0.5647058824, blue: 0.8862745098, alpha: 1), transitMode: TransitMode.bus)], location: CLLocation(latitude: 37.352516, longitude: -121.96947))]
 
         var results: [Station]? = [Station]()
 
@@ -66,7 +74,7 @@ class HereTests: XCTestCase {
         }
         wait(for: [ex], timeout: 5)
         XCTAssertNotNil(results!) // TODO: FIX
-//        XCTAssertEqual(results, [station])
+        XCTAssertEqual(results, "Warm Springs/South Fremont")
     }
 
     func testGetLine() {
@@ -83,5 +91,52 @@ class HereTests: XCTestCase {
         wait(for: [ex], timeout: 5)
 //        XCTAssertEqual(results!, )
 //        XCTAssertEqual(, )
+    }
+
+    func testParseStationForId() {
+        let resJson = testJson["Res"] as! [String: Any]
+        let stationsJson = resJson["Stations"] as! [String: Any]
+        let stnsJson = stationsJson["Stn"] as! [[String: Any]]
+
+        let test = Here().parseStationForId(from: stnsJson[0])
+
+        XCTAssertEqual(test, 718_290_976)
+    }
+
+    func testParseOperatorFromStationId() {
+        let resJson = testJsonForAgency["Res"] as! [String: Any]
+        let multiNextDepartures = resJson["MultiNextDepartures"] as! [String: Any]
+        let multiNextDeparture = multiNextDepartures["MultiNextDeparture"] as! [[String: Any]]
+
+        let test = Here().parseOperatorFromStationId(from: multiNextDeparture[0])
+
+        XCTAssertEqual(test, Agency.SamsTrans)
+    }
+
+    func testParseStation() {
+        let loc = CLLocation(latitude: -122.5353, longitude: 37.935921)
+        let resJson = testJson["Res"] as! [String: Any]
+        let stationsJson = resJson["Stations"] as! [String: Any]
+        let stnsJson = stationsJson["Stn"] as! [[String: Any]]
+
+        let test = Here().parseStation(from: stnsJson[0])
+
+        XCTAssertEqual(test?.code, 718_290_976)
+        XCTAssertEqual(test?.name, "Magnolia Ave & Ward St")
+        XCTAssertNotNil(test?.lines)
+        XCTAssertEqual(test?.transitModes, [TransitMode.bus])
+        XCTAssertNotNil(test?.location)
+    }
+
+    func testTransitModeConvert() {
+        let num = 4
+        let num1 = 5
+        let num2 = 3
+        let num3 = 8
+
+        XCTAssertEqual(Here().transitModeConvert(num: num), TransitMode.lightRail)
+        XCTAssertEqual(Here().transitModeConvert(num: num1), TransitMode.bus)
+        XCTAssertEqual(Here().transitModeConvert(num: num2), TransitMode.calTrain)
+        XCTAssertEqual(Here().transitModeConvert(num: num3), TransitMode.bart)
     }
 }
