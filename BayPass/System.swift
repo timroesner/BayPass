fb76eadb6a49fbf78fba67b58af9c590a5bbd455
Ayesha Khan <akhan1231@hotmail.com>
2019-04-09 12:02:24 -0700

    Added Time to format in Here API's format

--- BayPass/System.swift
+++ BayPass/System.swift
@@ -16,14 +16,15 @@ class System {
     var allStations = [Station]()
     var allLines = [String: Line]()
     private var coordinates = [Coordinates]()
+    var time = Here.shared.getCurrentTimetoFormattedStringForHereAPI()
 
     func getAllStations() {
-        let c1 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.309690, longitude: -121.940223), radius: 1500, max: 50, time: "2019-06-24T07:30:00")
-        let c2 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.338489, longitude: -121.945763), radius: 1500, max: 50, time: "2019-06-24T07:30:00")
-        let c3 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.320425, longitude: -121.907463), radius: 1500, max: 50, time: "2019-06-24T07:30:00")
-        let c4 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.341991, longitude: -121.877350), radius: 1500, max: 50, time: "2019-06-24T07:30:00")
-        let c5 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.311733, longitude: -121.874423), radius: 1500, max: 50, time: "2019-06-24T07:30:00")
-        let c6 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.692235, longitude: -122.462882), radius: 1500, max: 50, time: "2019-06-24T07:30:00")
+        let c1 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.309690, longitude: -121.940223), radius: 1500, max: 50, time: time)
+        let c2 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.338489, longitude: -121.945763), radius: 1500, max: 50, time: time)
+        let c3 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.320425, longitude: -121.907463), radius: 1500, max: 50, time: time)
+        let c4 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.341991, longitude: -121.877350), radius: 1500, max: 50, time: time)
+        let c5 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.311733, longitude: -121.874423), radius: 1500, max: 50, time: time)
+        let c6 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.692235, longitude: -122.462882), radius: 1500, max: 50, time: time)
 
         coordinates.append(c1)
         coordinates.append(c2)
