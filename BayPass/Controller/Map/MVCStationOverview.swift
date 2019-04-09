fefc1aac565c3c604f9b051cfa058b59fbf774da
Ayesha Khan <akhan1231@hotmail.com>
2019-04-09 10:18:16 -0700

    Deleted not-needed code from MVC func

--- BayPass/Controller/Map/MVCStationOverview.swift
+++ BayPass/Controller/Map/MVCStationOverview.swift
@@ -11,17 +11,6 @@ import UIKit
 
 extension MapViewController {
     func displayStationInfo(to station: Station) {
-        var userLocation: CLLocationCoordinate2D
-        if ProcessInfo.processInfo.arguments.contains("UITests") {
-            userLocation = CLLocationCoordinate2D(latitude: 37.331348, longitude: -121.888877)
-        } else {
-            guard let realUserLocation = locationManager.location?.coordinate else {
-                displayAlert(title: "User Location unknown", msg: "Please allow BayPass to access your current location, to plan a route to this destination.", dismissAfter: false)
-                return
-            }
-            userLocation = realUserLocation
-        }
-
         bottomSheet.moveOverlay(toNotchAt: 0, animated: true)
         removeChild(bottomSheet)
         mapView.removeAnnotations(mapView.annotations)
