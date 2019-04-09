c5b6d5920dee94201181d1dc5cde04734a1cacf2
Ayesha Khan <akhan1231@hotmail.com>
2019-04-08 23:20:53 -0700

    Added another route for routeOverViewTests

--- BayPassTests/View/RouteOverViewTests.swift
+++ BayPassTests/View/RouteOverViewTests.swift
@@ -35,4 +35,18 @@ class RouteOverViewTests: XCTestCase {
 //        XCTAssertEqual(subject.durationLabel.text, String(depTime.duration(to: arrTime)))
         XCTAssertNotNil(subject)
     }
+
+    func testSetupLineViewWalking() {
+        let depTime = Date(timeIntervalSinceNow: 60)
+        let arrTime = Date(timeIntervalSinceNow: 240)
+        let polyline = MKPolyline(coordinates: [])
+        let line = Line(name: "522", agency: Agency.ACE, destination: "Palo Alto", color: #colorLiteral(red: 0.2901960784, green: 0.5647058824, blue: 0.8862745098, alpha: 1), transitMode: TransitMode.bus)
+        let firstStop = Station(name: "Santa Clara & 5th", code: 320, transitModes: [TransitMode.bus], lines: [line], location: CLLocation(latitude: 37.34, longitude: -121.89))
+        let waypoints = [firstStop]
+        let segment = RouteSegment(distanceInMeters: 3000, departureTime: depTime, arrivalTime: arrTime, polyline: polyline, travelMode: TravelMode.walking, line: line, price: 2.25, waypoints: waypoints)
+
+        let route = Route(departureTime: depTime, arrivalTime: arrTime, segments: [segment])
+        let subject = RouteOverView(with: route)
+        XCTAssertNotNil(subject)
+    }
 }
