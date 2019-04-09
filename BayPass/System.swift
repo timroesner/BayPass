5ce94212fa11e1e8c5a7debb7055602992760993
Ayesha Khan <akhan1231@hotmail.com>
2019-04-09 11:09:22 -0700

    Change time

--- BayPass/System.swift
+++ BayPass/System.swift
@@ -18,12 +18,12 @@ class System {
     private var coordinates = [Coordinates]()
 
     func getAllStations() {
-        let c1 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.309690, longitude: -121.940223), radius: 1500, max: 50, time: "2019-03-24T01:23:45")
-        let c2 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.338489, longitude: -121.945763), radius: 1500, max: 50, time: "2019-03-24T01:23:45")
-        let c3 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.320425, longitude: -121.907463), radius: 1500, max: 50, time: "2019-03-24T01:23:45")
-        let c4 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.341991, longitude: -121.877350), radius: 1500, max: 50, time: "2019-03-24T01:23:45")
-        let c5 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.311733, longitude: -121.874423), radius: 1500, max: 50, time: "2019-03-24T01:23:45")
-        let c6 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.692235, longitude: -122.462882), radius: 1500, max: 50, time: "2019-03-24T01:23:45")
+        let c1 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.309690, longitude: -121.940223), radius: 1500, max: 50, time: "2019-06-24T07:30:00")
+        let c2 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.338489, longitude: -121.945763), radius: 1500, max: 50, time: "2019-06-24T07:30:00")
+        let c3 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.320425, longitude: -121.907463), radius: 1500, max: 50, time: "2019-06-24T07:30:00")
+        let c4 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.341991, longitude: -121.877350), radius: 1500, max: 50, time: "2019-06-24T07:30:00")
+        let c5 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.311733, longitude: -121.874423), radius: 1500, max: 50, time: "2019-06-24T07:30:00")
+        let c6 = Coordinates(center: CLLocationCoordinate2D(latitude: 37.692235, longitude: -122.462882), radius: 1500, max: 50, time: "2019-06-24T07:30:00")
 
         coordinates.append(c1)
         coordinates.append(c2)
@@ -43,6 +43,7 @@ class System {
                     }
                     for line in station.lines {
                         self.allLines[line.name + "-" + line.agency.stringValue] = line
+                        print(self.allLines)
                     }
                 }
                 self.group.leave()
