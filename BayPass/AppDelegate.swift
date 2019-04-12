077d044e87c9478e0385b243ea9a88a353cf1f5a
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-04-11 20:39:47 -0700

    Save and load user data on terminate and start respectively

--- BayPass/AppDelegate.swift
+++ BayPass/AppDelegate.swift
@@ -36,8 +36,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        UserManager.shared.save()
     }
 
     func applicationWillEnterForeground(_: UIApplication) {
@@ -45,7 +44,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(_: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        UserManager.shared.load()
     }
 
     func applicationWillTerminate(_: UIApplication) {
