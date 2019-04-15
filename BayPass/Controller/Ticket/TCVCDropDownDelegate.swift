7e9dc25d52ca93ec205b330cb9bb717a8dacc05b
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-04-14 17:25:55 -0700

    Implement DropDownDelegate in TicketCheckoutVC

---
+++ BayPass/Controller/Ticket/TCVCDropDownDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  TCVCDropDownDelegate.swift
+//  BayPass
+//
+//  Created by Tim Roesner on 4/14/19.
+//  Copyright © 2019 Tim Roesner. All rights reserved.
+//
+
+import UIKit
+
+extension TicketCheckoutViewController: DropDownDelegate {
+    func didChangeSelectedItem(_ sender: DropDownMenu) {
+        if sender.titleLbl.text != "PAYMENT METHOD" {
+            switch agency {
+            case .BART:
+                updateBARTTicketPrice()
+            default:
+                break
+            }
+        }
+    }
+    
+    func updateBARTTicketPrice() {
+        guard let type = stackedViews[safe: 1] as? DropDownMenu,
+            let fromDropDown = stackedViews[safe: 2] as? DropDownMenu,
+            let toDropDown = stackedViews[safe: 3] as? DropDownMenu
+        else { return }
+        
+        TicketManager.shared.getBARTPrice(from: fromDropDown.getSelectedItem(), to: toDropDown.getSelectedItem()) { (newPrice) in
+            let calculatedPrice = type.getSelectedItem() == "Roundtrip" ? newPrice*2 : newPrice
+            self.currentTicketPrice = calculatedPrice
+            self.payButton?.setTitle(String(format:"Pay $%.2f", calculatedPrice), for: .normal)
+        }
+    }
+}
