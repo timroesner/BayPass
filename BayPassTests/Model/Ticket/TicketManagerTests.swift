4e8422827d13ad08162600abf9a0ae9b6f3fa66e
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-04-22 22:36:08 -0700

    Add more tests for TicketManager

--- BayPassTests/Model/Ticket/TicketManagerTests.swift
+++ BayPassTests/Model/Ticket/TicketManagerTests.swift
@@ -12,7 +12,7 @@ import XCTest
 class TicketManagerTests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+       
     }
 
     override func tearDown() {
@@ -22,6 +22,31 @@ class TicketManagerTests: XCTestCase {
     func testCalTrainZonesFailure() {
         let price = TicketManager.shared.getCalTrainPrice(ticketType: "Single Ride", from: "", to: "")
         XCTAssertNil(price)
+    }
+    
+    func testCalTrainPrice() {
+        let price = TicketManager.shared.getCalTrainPrice(ticketType: "Single Ride", from: "San Jose Diridon", to: "San Francisco")
+        XCTAssertEqual(price, 9.95)
+    }
+    
+    func testDropDownOptionsCalTrain() {
+        let options = TicketManager.shared.getDropDownOptions(for: .CalTrain, onlyPasses: false)
+        XCTAssertEqual(options.count, 4)
+    }
+    
+    func testDropDownOptionsVTA() {
+        let options = TicketManager.shared.getDropDownOptions(for: .VTA, onlyPasses: true)
+        XCTAssertEqual(options.count, 3)
+    }
+    
+    func testPriceVTA() {
+        let price = TicketManager.shared.getTicketPrice(agency: .VTA, ticketType: "Single Ride", subType: "Adult")
+        XCTAssertEqual(price, 2.50)
+    }
+    
+    func testPriceMuni() {
+        let price = TicketManager.shared.getTicketPrice(agency: .Muni, ticketType: "Day Pass", subType: nil)
+        XCTAssertEqual(price, 5.00)
     }
 
 }
