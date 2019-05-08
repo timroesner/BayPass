d53d72b432bb09325f28d5be033f1c1992f9dcd5
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-05-07 21:35:45 -0700

    Add test for Apple Pay delegate methods

--- BayPassTests/Controller/TicketViewControllerTests.swift
+++ BayPassTests/Controller/TicketViewControllerTests.swift
@@ -8,6 +8,7 @@
 
 @testable import BayPass
 import XCTest
+import Stripe
 
 class TicketViewControllerTests: XCTestCase {
     
@@ -46,6 +47,42 @@ class TicketViewControllerTests: XCTestCase {
         XCTAssertNotNil(UIApplication.shared.keyWindow?.rootViewController)
         vc.updateTicketPrice()
         vc.pay()
+    }
+    
+    func testCheckoutDropDownBART() {
+        let vc = TicketCheckoutViewController()
+        vc.agency = .BART
+        UIApplication.shared.keyWindow!.rootViewController = vc
+        XCTAssertNotNil(UIApplication.shared.keyWindow?.rootViewController)
+        vc.updateBARTTicketPrice()
+    }
+    
+    func testDidFinish() {
+        let vc = TicketCheckoutViewController()
+        let request = Stripe.paymentRequest(withMerchantIdentifier: Credentials().merchantId, country: "US", currency: "USD")
+        request.paymentSummaryItems = [PKPaymentSummaryItem(label: "TestItem", amount: NSDecimalNumber(value: 5.00))]
+        let paymentVC = PKPaymentAuthorizationViewController(paymentRequest: request)!
+        vc.paymentAuthorizationViewControllerDidFinish(paymentVC)
+        
+        let presentedVC = UIApplication.shared.keyWindow?.rootViewController?.presentedViewController
+        XCTAssertFalse(presentedVC is TicketCheckoutViewController)
+    }
+    
+    func testDidAuthorize() {
+        let vc = TicketCheckoutViewController()
+        let request = Stripe.paymentRequest(withMerchantIdentifier: Credentials().merchantId, country: "US", currency: "USD")
+        request.paymentSummaryItems = [PKPaymentSummaryItem(label: "TestItem", amount: NSDecimalNumber(value: 5.00))]
+        let paymentVC = PKPaymentAuthorizationViewController(paymentRequest: request)!
+        let payment = PKPayment()
+        
+        let expectation = self.expectation(description: "async")
+        var statusResult = PKPaymentAuthorizationStatus(rawValue: 0)
+        vc.paymentAuthorizationViewController(paymentVC, didAuthorizePayment: payment) {
+            statusResult = $0
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+        print(statusResult as Any)
     }
 
 }
