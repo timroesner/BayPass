df294165b66917ed58548433f7f90c6cb1a6e5d6
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-05-09 23:13:06 -0700

    Fetch correct station coordinate and set Map rect for station

--- BayPass/Controller/Map/MVCStationOverview.swift
+++ BayPass/Controller/Map/MVCStationOverview.swift
@@ -14,22 +14,19 @@ extension MapViewController {
         bottomSheet.moveOverlay(toNotchAt: 0, animated: true)
         mapView.removeAnnotations(mapView.annotations)
 
-        let coordinate = CLLocationCoordinate2D(latitude: station.location.coordinate.latitude, longitude: station.location.coordinate.longitude)
-        let mapEdgePadding = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
-
-        let coordinatRegion = MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
         mapView.addAnnotation(MapAnnotation(fromStation: station))
-
-//        mapView.setRegion(mapView.regionThatFits(coordinatRegion), animated: true)
-//        mapView.setVisibleMapRect(coordinatRegion, edgePadding: mapEdgePadding, animated: true)
-        stationVC.station = station
-        stationVC.cancelLabel.isUserInteractionEnabled = true
+        let insets = UIEdgeInsets(top: 0, left: view.frame.width/2, bottom: 0, right: 0)
+        let rect = MKMapRect(origin: MKMapPoint(station.location.coordinate), size: MKMapSize(width: 3000, height: 3000))
+        mapView.setVisibleMapRect(rect, edgePadding: insets, animated: true)
+        
+        let stationVC = StationViewController(station: station)
         let tap = UITapGestureRecognizer(target: self, action: #selector(tapFunction))
-        stationVC.cancelLabel.addGestureRecognizer(tap)
+        stationVC.cancelView.addGestureRecognizer(tap)
+        stationVC.parentMapVC = self
         bottomSheet.invalidateNotchHeights()
-        notchPercentages = [0.175, 0.5, 0.95]
+        notchPercentages = [0.19, 0.5, 0.95]
         bottomSheet.viewControllers = [stationVC]
-        addChild(bottomSheet, in: view)
+        bottomSheet.drivingScrollView = stationVC.lineTableView
         bottomSheet.moveOverlay(toNotchAt: 1, animated: true)
     }
 
