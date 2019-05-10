7c0b19564613d81e5e9eebd475dca701f4b79737
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-05-09 23:13:27 -0700

    Restructure StationViewController

--- BayPass/Controller/Map/StationViewController.swift
+++ BayPass/Controller/Map/StationViewController.swift
@@ -6,138 +6,137 @@
 //  Copyright © 2019 Tim Roesner. All rights reserved.
 //
 
-import OverlayContainer
 import SnapKit
 import UIKit
+import MapKit
 
 class StationViewController: UIViewController {
-    var myTableView = UITableView()
-    var station: Station?
-    var lines: [Line]?
-    var linesTimeDict = [[String: String]]()
-    let searchVC = SearchViewController()
-
-    var cancelLabel: UIImageView = {
-        let image = UIImageView(frame: CGRect(x: 0, y: 0, width: 45, height: 45))
-        image.image = #imageLiteral(resourceName: "close")
-        return image
+    var station: Station
+    let lineTableView = UITableView()
+    let lineCellId = "lineCell"
+    let stationNameLabel = UILabel()
+    let stationImageView = UIImageView()
+    let getDirectionsButton = UIButton()
+    var cancelView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "close")
+        return imageView
     }()
-
+    var nextDepartures = [(line: Line, departureTimes: [Date])]()
+    var parentMapVC: MapViewController?
+    
+    init(station: Station) {
+        self.station = station
+        super.init(nibName: nil, bundle: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let group = DispatchGroup()
-        lines = station?.lines
-
-        DispatchQueue.main.async {
-            group.enter()
-            Here.shared.getDepartureTimesForAStation(stationId: self.station?.code ?? 0) { lineToTimesDict in
-                self.linesTimeDict = lineToTimesDict
-                print("🌬\(self.linesTimeDict)")
-                group.leave()
-            }
-        }
-        group.notify(queue: .main) {
-            print("🏖\(self.linesTimeDict)")
-        }
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.topAnchor.constraint(equalTo: view.topAnchor, constant: 500).isActive = true
-
         view.backgroundColor = UIColor.white
         view.layer.cornerRadius = 21
-        setupViews()
-        myTableView.reloadData()
+        view.layer.applySketchShadow(color: .black, alpha: 0.20, x: 6, y: 6, blur: 25, spread: 5)
+        
+        setUpConstraints()
+        setUpTableView()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        myTableView.reloadData()
+        setViewInfos()
+        fetchDepartureTimes()
+    }
+    
+    func setViewInfos() {
+        stationImageView.image = station.getIcon()
+        stationNameLabel.text = station.name.replacingOccurrences(of: "\\s?\\([\\w\\s]*\\)", with: "", options: .regularExpression)
     }
 
-    func setupViews() {
-        setUpTitle()
-        setUpTableView()
-    }
-
-    func setUpTitle() {
-        let stationName = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        stationName.text = station?.name.replacingOccurrences(of: "\\s?\\([\\w\\s]*\\)", with: "", options: .regularExpression) // Removes Parantheses
-        stationName.font = stationName.font.withSize(25) // TODO: Format so it automatically fits
-        stationName.font = UIFont.boldSystemFont(ofSize: 16)
-        stationName.numberOfLines = 0
-
-        let stationImage = station?.getIcon()
-        let stationImageView = UIImageView(image: stationImage)
+    func setUpConstraints() {
+        let moveIndicator = MoveIndicator()
+        view.addSubview(moveIndicator)
+        moveIndicator.setConstraints()
+        
         stationImageView.tintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-
-        view.addSubview(stationName)
         view.addSubview(stationImageView)
-        view.addSubview(cancelLabel)
-        view.addSubview(myTableView)
-
         stationImageView.snp.makeConstraints { make in
-            make.topMargin.left.equalTo(12)
-            make.topMargin.equalTo(18)
-        }
-        stationName.snp.makeConstraints { make in
-            make.left.equalTo(stationImageView.snp.right)
-            make.topMargin.equalTo(26)
-        }
-        cancelLabel.snp.makeConstraints { make in
-            make.topMargin.equalTo(20)
-            make.right.equalTo(-28)
+            make.top.equalToSuperview().offset(18)
+            make.left.equalToSuperview().offset(12)
+            make.width.height.equalTo(50)
         }
 
-        myTableView.snp.makeConstraints { make in
-            make.top.equalTo(stationImageView.snp.bottom).offset(5)
-            make.left.equalToSuperview().offset(20)
-            make.right.equalTo(-28)
-            make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
+        cancelView.isUserInteractionEnabled = true
+        view.addSubview(cancelView)
+        cancelView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(12)
+            make.right.equalToSuperview().inset(12)
+            make.width.height.equalTo(25)
         }
+        
+        stationNameLabel.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        view.addSubview(stationNameLabel)
+        stationNameLabel.snp.makeConstraints { make in
+            make.left.equalTo(stationImageView.snp.right).offset(2)
+            make.top.equalTo(stationImageView).offset(2)
+            make.right.equalTo(cancelView.snp.left).offset(4)
+        }
+        
+        getDirectionsButton.setTitle("Get Directions", for: .normal)
+        getDirectionsButton.setTitleColor(UIColor(hex: 0x007AFF), for: .normal)
+        getDirectionsButton.addTarget(self, action: #selector(getDirections), for: .touchUpInside)
+        view.addSubview(getDirectionsButton)
+        getDirectionsButton.snp.makeConstraints { (make) in
+            make.top.equalTo(stationNameLabel.snp.bottom).offset(2)
+            make.left.equalTo(stationNameLabel)
+            make.height.equalTo(20)
+        }
+
+        view.addSubview(lineTableView)
+        lineTableView.snp.makeConstraints { make in
+            make.top.equalTo(stationImageView.snp.bottom).offset(8)
+            make.left.right.equalToSuperview().inset(12)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
+    }
+    
+    func fetchDepartureTimes() {
+        Here.shared.getDepartureTimesForAStation(stationId: station.code) { results in
+            self.nextDepartures = results
+            self.nextDepartures.sort(by: { ($0.departureTimes.first ?? Date(timeIntervalSince1970: 0)) < ($1.departureTimes.first ?? Date(timeIntervalSince1970: 0)) })
+            self.lineTableView.reloadData()
+        }
+    }
+    
+    @objc func getDirections() {
+        let placemark = MKPlacemark(coordinate: station.location.coordinate, addressDictionary: nil)
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = station.name
+        parentMapVC?.displayRoute(to: mapItem)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        print("init(coder:) has not been implemented for StationViewController")
+        return nil
     }
 }
 
 extension StationViewController: UITableViewDelegate, UITableViewDataSource {
     func setUpTableView() {
-        myTableView.dataSource = self
-        myTableView.delegate = self
-        myTableView.separatorColor = .clear
-        myTableView.translatesAutoresizingMaskIntoConstraints = false
-        myTableView.rowHeight = UITableView.automaticDimension
-        myTableView.register(LineTableViewCell.self, forCellReuseIdentifier: "Cell")
-        myTableView.rowHeight = 80
-        myTableView.isScrollEnabled = true
-        myTableView.allowsSelection = false
+        lineTableView.dataSource = self
+        lineTableView.delegate = self
+        lineTableView.separatorColor = .clear
+        lineTableView.showsVerticalScrollIndicator = false
+        lineTableView.register(LineTableViewCell.self, forCellReuseIdentifier: lineCellId)
+        lineTableView.rowHeight = 80
+        lineTableView.allowsSelection = false
     }
 
     func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
-        return lines?.count ?? 0
+        return nextDepartures.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! LineTableViewCell
-        let defaultLine = Line(name: "m", agency: Agency.ACE, destination: "n", color: #colorLiteral(red: 0.2901960784, green: 0.5647058824, blue: 0.8862745098, alpha: 1), transitMode: TransitMode.bart)
-        cell.setup(with: lines?[indexPath.row] ?? defaultLine)
-        print("🔮getting \(String(describing: station?.code))")
-        print("🛒Date: \(Date().getCurrentTimetoFormattedStringForHereAPI())")
-        let nameAndDir = (lines?[indexPath.row].name)! + " to " + (lines?[indexPath.row].destination)!
-        let time = getTimeForLine(directionFromLine: nameAndDir)
-        cell.timeLabel.text = time
-        print("🤬\(time)")
+        let cell = tableView.dequeueReusableCell(withIdentifier: lineCellId) as! LineTableViewCell
+        cell.setup(with: nextDepartures[indexPath.row])
         return cell
-    }
-
-    func getTimeForLine(directionFromLine: String) -> String {
-        var time = ""
-        var dictionary = [String: String]()
-
-        for dict in linesTimeDict {
-            dictionary = dict.filter({ $0.key == directionFromLine })
-        }
-
-        print("🚀\(linesTimeDict)")
-        print("🍭\(dictionary)")
-        time = dictionary.values.description
-        print("⏰\(time)")
-        return time
     }
 }
